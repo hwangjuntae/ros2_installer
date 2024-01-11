e87feb2870641513093c6c1dcdce5318ed3ef3ed
hwangjuntae <jt2002120225@gmail.com>
2024-01-11 14:34:07 +0900

    complete foxy

--- ros2_installer_foxy.py
+++ ros2_installer_foxy.py
@@ -45,34 +45,42 @@ os.system(a15)
 
 # edit ros2 foxy ~/.bashrc
 
-os.system("echo source /opt/ros/foxy/setup.bash>> ~/.bashrc")
-os.system("echo alias cw='cd ~/ros2_ws'>> ~/.bashrc")
-os.system("echo alias cs='cd ~/ros2_ws/src'>> ~/.bashrc")
-os.system("echo alias ccd='colcon_cd'>> ~/.bashrc")
-os.system("echo export ROS_DOMAIN_ID=30>> ~/.bashrc")
-os.system("echo export ROS_NAMESPACE=robot1>> ~/.bashrc")
+# ~/.bashrc command
+
+os.system("echo \"alias eb='code ~/.bashrc'\" >> ~/.bashrc")
+os.system("echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc")
+
+os.system("echo \"source /opt/ros/foxy/setup.bash\" >> ~/.bashrc")
+os.system("echo \"alias cw='cd ~/ros2_ws'\" >> ~/.bashrc")
+os.system("echo \"alias cs='cd ~/ros2_ws/src'\" >> ~/.bashrc")
+os.system("echo \"alias ccd='colcon_cd'\" >> ~/.bashrc")
+os.system("echo \"export ROS_DOMAIN_ID=30\" >> ~/.bashrc")
+os.system("echo \"export ROS_NAMESPACE=robot1\" >> ~/.bashrc")
 
 # build
-os.system("echo alias cb='colcon build --symlink-install'>> ~/.bashrc")
-os.system("echo alias cbp='colcon build --symlink-install --packages-select'>> ~/.bashrc")
-os.system("echo alias cbu='colcon build --symlink-install --packages-up-to'>> ~/.bashrc")
-os.system("echo alias ct='colcon test'>> ~/.bashrc")
-os.system("echo alias ctp='colcon test --packages-select'>> ~/.bashrc")
-os.system("echo alias ctr='colcon test-result'>> ~/.bashrc")
+os.system("echo \"alias cb='colcon build --symlink-install'\" >> ~/.bashrc")
+os.system("echo \"alias cbp='colcon build --symlink-install --packages-select'\" >> ~/.bashrc")
+os.system("echo \"alias cbu='colcon build --symlink-install --packages-up-to'\" >> ~/.bashrc")
+os.system("echo \"alias ct='colcon test'\" >> ~/.bashrc")
+os.system("echo \"alias ctp='colcon test --packages-select'\" >> ~/.bashrc")
+os.system("echo \"alias ctr='colcon test-result'\" >> ~/.bashrc")
 
 # topic list
-os.system("echo alias rt='ros2 topic list'>> ~/.bashrc")
-os.system("echo alias re='ros2 topic echo'>> ~/.bashrc")
-os.system("echo alias rn='ros2 node list'>> ~/.bashrc")
+os.system("echo \"alias rt='ros2 topic list'\" >> ~/.bashrc")
+os.system("echo \"alias re='ros2 topic echo'\" >> ~/.bashrc")
+os.system("echo \"alias rn='ros2 node list'\" >> ~/.bashrc")
 
 # etc set
-os.system("echo alias kill='killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient'>> ~/.bashrc")
-os.system("echo alias af='ament_flake8'>> ~/.bashrc")
-os.system("echo alias ac='ament_cpplint'>> ~/.bashrc")
+os.system("echo \"alias kill='killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient'\" >> ~/.bashrc")
+os.system("echo \"alias af='ament_flake8'\" >> ~/.bashrc")
+os.system("echo \"alias ac='ament_cpplint'\" >> ~/.bashrc")
 
 # test
-os.system("echo alias testpub='ros2 run demo_nodes_cpp talker'>> ~/.bashrc")
-os.system("echo alias testsub='ros2 run demo_nodes_cpp listener'>> ~/.bashrc")
-os.system("echo alias testpubimg='ros2 run image_tools cam2image'>> ~/.bashrc")
-os.system("echo alias testsubimg='ros2 run image_tools showimage'>> ~/.bashrc")
+os.system("echo \"alias testpub='ros2 run demo_nodes_cpp talker'\" >> ~/.bashrc")
+os.system("echo \"alias testsub='ros2 run demo_nodes_cpp listener'\" >> ~/.bashrc")
+os.system("echo \"alias testpubimg='ros2 run image_tools cam2image'\" >> ~/.bashrc")
+os.system("echo \"alias testsubimg='ros2 run image_tools showimage'\" >> ~/.bashrc")
+
+# os.system()
+
 
