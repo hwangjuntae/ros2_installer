53cf108fa8100007a72672ee100a185049d63bae
hwangjuntae <jt2002120225@gmail.com>
2024-01-11 13:21:13 +0900

    all auto

--- ros2_installer_foxy.py
+++ ros2_installer_foxy.py
@@ -3,16 +3,16 @@
 import os, sys
 
 #set locale
-a1 = "sudo apt update && sudo apt install locales"
+a1 = "sudo apt update -y && sudo apt install locales"
 a2 = "sudo locale-gen en_US en_US.UTF-8"
 a3 = "sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8"
 a4 = "export LANG=en_US.UTF-8"
 a5 = "locale"
 
 #setup sources
-a6 = "sudo apt install software-properties-common"
+a6 = "sudo apt install software-properties-common -y"
 a7 = "sudo add-apt-repository universe"
-a8 = "sudo apt update && sudo apt install curl"
+a8 = "sudo apt update -y && sudo apt install curl -y"
 a9 = "sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg"
 
 temp = "\"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main\""
@@ -22,9 +22,10 @@ a10 = "echo " + temp + " | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/nul
 
 #install ros2 packages
 a11 = "dpkg --get-selections | grep hold"
-a12 = "sudo apt update && sudo apt upgrade"
-a13 = "sudo apt install ros-foxy-desktop python3-argcomplete"
-a14 = "sudo apt install ros-dev-tools"
+a12 = "sudo apt update -y && sudo apt upgrade -y"
+a13 = "sudo apt install ros-foxy-desktop python3-argcomplete -y"
+a14 = "sudo apt install ros-dev-tools -y"
+a15 = "sudo apt install python3-colcon-common-extensions -y"
 
 os.system(a1)
 os.system(a2)
@@ -40,6 +41,7 @@ os.system(a11)
 os.system(a12)
 os.system(a13)
 os.system(a14)
+os.system(a15)
 
 # edit ros2 foxy ~/.bashrc
 
@@ -73,3 +75,4 @@ os.system(echo "alias testpub='ros2 run demo_nodes_cpp talker'>> .bashrc")
 os.system(echo "alias testsub='ros2 run demo_nodes_cpp listener'>> .bashrc")
 os.system(echo "alias testpubimg='ros2 run image_tools cam2image'>> .bashrc")
 os.system(echo "alias testsubimg='ros2 run image_tools showimage'>> .bashrc")
+
