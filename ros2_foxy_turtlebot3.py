440a8774199039c77faa60113b0beaa8a458cafc
hwangjuntae <jt2002120225@gmail.com>
2024-01-17 21:48:56 +0900

    edit foxy turtlebot3

--- ros2_foxy_turtlebot3.py
+++ ros2_foxy_turtlebot3.py
@@ -1,13 +1,13 @@
 import os, sys
 
-os.system("sudo apt-get install ros-foxy-gazebo-*")
-os.system("sudo apt install ros-foxy-cartographer")
-os.system("sudo apt install ros-foxy-cartographer-ros")
-os.system("sudo apt install ros-foxy-navigation2")
-os.system("sudo apt install ros-foxy-nav2-bringup")
-os.system("sudo apt install ros-foxy-dynamixel-sdk")
-os.system("sudo apt install ros-foxy-turtlebot3-msgs")
-os.system("sudo apt install ros-foxy-turtlebot3")
+os.system("sudo apt-get install ros-foxy-gazebo-* -y")
+os.system("sudo apt install ros-foxy-cartographer -y")
+os.system("sudo apt install ros-foxy-cartographer-ros -y")
+os.system("sudo apt install ros-foxy-navigation2 -y")
+os.system("sudo apt install ros-foxy-nav2-bringup -y")
+os.system("sudo apt install ros-foxy-dynamixel-sdk -y")
+os.system("sudo apt install ros-foxy-turtlebot3-msgs -y")
+os.system("sudo apt install ros-foxy-turtlebot3 -y")
 os.system("echo \'export ROS_DOMAIN_ID=30 #TURTLEBOT3\' >> ~/.bashrc")
 
 # os.system("")
