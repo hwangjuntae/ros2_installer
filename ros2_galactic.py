07ee531db8dfc605b3e827c13d8342af39c0cff0
hwangjuntae <jt2002120225@gmail.com>
2024-03-28 15:38:38 +0900

    add galactic

--- ros2_galactic.py
+++ ros2_galactic.py
@@ -3,42 +3,24 @@
 import os
 
 os.system("locale")
+
 os.system("sudo apt update && sudo apt install locales")
 os.system("sudo locale-gen en_US en_US.UTF-8")
 os.system("sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8")
 os.system("export LANG=en_US.UTF-8")
+
 os.system("locale")
 
 os.system("sudo apt install software-properties-common")
 os.system("sudo add-apt-repository universe")
-os.system("sudo apt update && sudo apt install curl -y")
-os.system("sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg -y ")
+
+os.system("sudo apt update && sudo apt install curl")
+os.system("sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg")
+
 os.system("echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main\" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null")
-os.system("sudo apt update && sudo apt install -y")
-os.system("sudo apt install libbullet-dev -y")
-os.system("sudo apt install python3-pip -y")
-os.system("sudo apt install python3-pytest-cov -y")
-os.system("sudo apt install ros-dev-tools -y")
 
-# install some pip packages needed for testing
-os.system("python3 -m pip install -U argcomplete")
-os.system("python3 -m pip install -U flake8-blind-except")
-os.system("python3 -m pip install -U flake8-builtins")
-os.system("python3 -m pip install -U flake8-class-newline")
-os.system("python3 -m pip install -U flake8-comprehensions")
-os.system("python3 -m pip install -U flake8-deprecated")
-os.system("python3 -m pip install -U flake8-docstrings")
-os.system("python3 -m pip install -U flake8-import-order")
-os.system("python3 -m pip install -U flake8-quotes")
-os.system("python3 -m pip install -U pytest-repeat")
-os.system("python3 -m pip install -U pytest-rerunfailures")
-os.system("python3 -m pip install -U pytest")
-# install Fast-RTPS dependencies
-os.system("sudo apt install --no-install-recommendslibasio-dev")
-os.system("sudo apt install --no-install-recommendslibtinyxml2-dev")
-# install Cyclone DDS dependencies
-os.system("sudo apt install --no-install-recommends libcunit1-dev")
-
+os.system("sudo apt update -y && sudo apt upgrade -y")
+os.system("sudo apt install ros-galactic-desktop -y")
 
 #---------------------------------------------------------------------------------------------------------------------------
 # edit ros2 foxy ~/.bashrc
@@ -48,8 +30,8 @@ os.system("sudo apt install --no-install-recommends libcunit1-dev")
 os.system("echo \"alias eb='code ~/.bashrc'\" >> ~/.bashrc")
 os.system("echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc")
 
-os.system("echo \"source /opt/ros/foxy/setup.bash\" >> ~/.bashrc")
-os.system("echo \"alias ss='. install/setup.bash\"")
+os.system("echo \"source /opt/ros/galatic/setup.bash\" >> ~/.bashrc")
+os.system("echo \"alias ss='. install/setup.bash\" >> ~/.bashrc")
 os.system("echo \"alias cw='cd ~/ros2_ws'\" >> ~/.bashrc")
 os.system("echo \"alias cs='cd ~/ros2_ws/src'\" >> ~/.bashrc")
 os.system("echo \"alias ccd='colcon_cd'\" >> ~/.bashrc")
