858ce327e87805837875bdf3490d7bca3c06682c
hwangjuntae <jt2002120225@gmail.com>
2024-06-01 02:52:45 +0900

    edit galactic desktop -> ros-base

--- ros2_galactic.py
+++ ros2_galactic.py
@@ -2,27 +2,25 @@
 
 import os
 
-os.system("sudo apt install curl -y")
-
 os.system("locale")
 
-os.system("sudo apt update && sudo apt install locales")
+os.system("sudo apt update && sudo apt install locales -y")
 os.system("sudo locale-gen en_US en_US.UTF-8")
 os.system("sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8")
 os.system("export LANG=en_US.UTF-8")
 
 os.system("locale")
 
-os.system("sudo apt install software-properties-common")
+os.system("sudo apt install software-properties-common -y")
 os.system("sudo add-apt-repository universe")
 
-os.system("sudo apt update && sudo apt install curl")
+os.system("sudo apt update && sudo apt install curl -y")
 os.system("sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg")
 
 os.system("echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main\" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null")
 
 os.system("sudo apt update -y && sudo apt upgrade -y")
-os.system("sudo apt install ros-galactic-desktop -y")
+os.system("sudo apt install ros-galactic-ros-base -y")
 os.system("sudo apt install python3-colcon-common-extensions")
 
 #---------------------------------------------------------------------------------------------------------------------------
